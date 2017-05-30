46e806c6dfb529618c2fb169ae53413d3878ff3a
johnkorn <koryagin.evgeniy@gmail.com>
2017-05-30 15:15:28 +0200

    rnn data preproc

---
+++ rnn_data_preparation.py
@@ -0,0 +1,193 @@
+import os
+import re
+import csv
+import codecs
+import numpy as np
+import pandas as pd
+
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+from string import punctuation
+
+from gensim.models import KeyedVectors
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.layers import Dense, Input, LSTM, Embedding, Dropout, Activation
+from keras.layers.merge import concatenate
+from keras.models import Model
+from keras.layers.normalization import BatchNormalization
+from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
+import tensorflow as tf
+
+import sys
+
+import keras.backend.tensorflow_backend as KTF
+import keras.backend as K
+
+import pickle
+
+########################################
+## set directories and parameters
+########################################
+BASE_DIR = 'DATA/'
+EMBEDDING_FILE = '/media/johnkorn/New Volume/DATA/EMBEDDINGS/GoogleNews-vectors-negative300.bin'
+#EMBEDDING_FILE = BASE_DIR + 'GoogleNews-vectors-negative300.bin'
+
+TRAIN_DATA_FILE = BASE_DIR + 'train.csv'
+TEST_DATA_FILE = BASE_DIR + 'test.csv'
+MAX_SEQUENCE_LENGTH = 30
+MAX_NB_WORDS = 200000
+EMBEDDING_DIM = 300
+VALIDATION_SPLIT = 0.1
+
+num_lstm = 256 #np.random.randint(175, 275)
+num_dense = 256 #np.random.randint(100, 150)
+rate_drop_lstm = 0.5 #0.15 + np.random.rand() * 0.25
+rate_drop_dense = 0.5 #0.15 + np.random.rand() * 0.25
+
+act = 'relu'
+re_weight = True # whether to re-weight classes to fit the 17.5% share in test set
+
+STAMP = 'lstm_%d_%d_%.2f_%.2f'%(num_lstm, num_dense, rate_drop_lstm, rate_drop_dense)
+
+########################################
+## index word vectors
+########################################
+print('Indexing word vectors')
+
+word2vec = KeyedVectors.load_word2vec_format(EMBEDDING_FILE, binary=True)
+print('Found %s word vectors of word2vec' % len(word2vec.vocab))
+
+########################################
+## process texts in datasets
+########################################
+print('Processing text dataset')
+
+
+# The function "text_to_wordlist" is from
+# https://www.kaggle.com/currie32/quora-question-pairs/the-importance-of-cleaning-text
+def text_to_wordlist(text, remove_stopwords=False, stem_words=False):
+    # Clean the text, with the option to remove stopwords and to stem words.
+
+    # Convert words to lower case and split them
+    text = text.lower().split()
+
+    # Optionally, remove stop words
+    if remove_stopwords:
+        stops = set(stopwords.words("english"))
+        text = [w for w in text if not w in stops]
+
+    text = " ".join(text)
+
+    # Clean the text
+    text = re.sub(r"[^A-Za-z0-9^,!.\/'+-=]", " ", text)
+    text = re.sub(r"what's", "what is ", text)
+    text = re.sub(r"\'s", " ", text)
+    text = re.sub(r"\'ve", " have ", text)
+    text = re.sub(r"can't", "cannot ", text)
+    text = re.sub(r"n't", " not ", text)
+    text = re.sub(r"i'm", "i am ", text)
+    text = re.sub(r"\'re", " are ", text)
+    text = re.sub(r"\'d", " would ", text)
+    text = re.sub(r"\'ll", " will ", text)
+    text = re.sub(r",", " ", text)
+    text = re.sub(r"\.", " ", text)
+    text = re.sub(r"!", " ! ", text)
+    text = re.sub(r"\/", " ", text)
+    text = re.sub(r"\^", " ^ ", text)
+    text = re.sub(r"\+", " + ", text)
+    text = re.sub(r"\-", " - ", text)
+    text = re.sub(r"\=", " = ", text)
+    text = re.sub(r"'", " ", text)
+    text = re.sub(r"(\d+)(k)", r"\g<1>000", text)
+    text = re.sub(r":", " : ", text)
+    text = re.sub(r" e g ", " eg ", text)
+    text = re.sub(r" b g ", " bg ", text)
+    text = re.sub(r" u s ", " american ", text)
+    text = re.sub(r"\0s", "0", text)
+    text = re.sub(r" 9 11 ", "911", text)
+    text = re.sub(r"e - mail", "email", text)
+    text = re.sub(r"j k", "jk", text)
+    text = re.sub(r"\s{2,}", " ", text)
+
+    # Optionally, shorten words to their stems
+    if stem_words:
+        text = text.split()
+        stemmer = SnowballStemmer('english')
+        stemmed_words = [stemmer.stem(word) for word in text]
+        text = " ".join(stemmed_words)
+
+    # Return a list of words
+    return (text)
+
+
+texts_1 = []
+texts_2 = []
+labels = []
+with codecs.open(TRAIN_DATA_FILE, encoding='utf-8') as f:
+    reader = csv.reader(f, delimiter=',')
+    header = next(reader)
+    for values in reader:
+        texts_1.append(text_to_wordlist(values[3]))
+        texts_2.append(text_to_wordlist(values[4]))
+        labels.append(int(values[5]))
+print('Found %s texts in train.csv' % len(texts_1))
+
+test_texts_1 = []
+test_texts_2 = []
+test_ids = []
+with codecs.open(TEST_DATA_FILE, encoding='utf-8') as f:
+    reader = csv.reader(f, delimiter=',')
+    header = next(reader)
+    for values in reader:
+        test_texts_1.append(text_to_wordlist(values[1]))
+        test_texts_2.append(text_to_wordlist(values[2]))
+        test_ids.append(values[0])
+print('Found %s texts in test.csv' % len(test_texts_1))
+
+tokenizer = Tokenizer(num_words=MAX_NB_WORDS)
+tokenizer.fit_on_texts(texts_1 + texts_2 + test_texts_1 + test_texts_2)
+
+sequences_1 = tokenizer.texts_to_sequences(texts_1)
+sequences_2 = tokenizer.texts_to_sequences(texts_2)
+test_sequences_1 = tokenizer.texts_to_sequences(test_texts_1)
+test_sequences_2 = tokenizer.texts_to_sequences(test_texts_2)
+
+word_index = tokenizer.word_index
+print('Found %s unique tokens' % len(word_index))
+
+data_1 = pad_sequences(sequences_1, maxlen=MAX_SEQUENCE_LENGTH)
+data_2 = pad_sequences(sequences_2, maxlen=MAX_SEQUENCE_LENGTH)
+labels = np.array(labels)
+print('Shape of data tensor:', data_1.shape)
+print('Shape of label tensor:', labels.shape)
+
+pickle.dump(data_1,'DATA/processed/traindata1.pkl')
+pickle.dump(data_2,'DATA/processed/traindata2.pkl')
+pickle.dump(labels,'DATA/processed/trainlabels.pkl')
+
+
+test_data_1 = pad_sequences(test_sequences_1, maxlen=MAX_SEQUENCE_LENGTH)
+test_data_2 = pad_sequences(test_sequences_2, maxlen=MAX_SEQUENCE_LENGTH)
+test_ids = np.array(test_ids)
+
+pickle.dump(test_data_1,'DATA/processed/testdata1.pkl')
+pickle.dump(test_data_2,'DATA/processed/testdata2.pkl')
+pickle.dump(test_ids,'DATA/processed/testids.pkl')
+
+########################################
+## prepare embeddings
+########################################
+print('Preparing embedding matrix')
+
+nb_words = min(MAX_NB_WORDS, len(word_index))+1
+
+embedding_matrix = np.zeros((nb_words, EMBEDDING_DIM))
+for word, i in word_index.items():
+    if word in word2vec.vocab:
+        embedding_matrix[i] = word2vec.word_vec(word)
+print('Null word embeddings: %d' % np.sum(np.sum(embedding_matrix, axis=1) == 0))
+
+pickle.dump(embedding_matrix, 'DATA/processed/w2vmatrix.pkl')
+
+print('Data processing finished!')
